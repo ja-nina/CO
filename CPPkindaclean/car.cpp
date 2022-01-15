07c2ec74cb298fb7da5e44f598ed6fb1a94092fc
MatKadd <82376286+MatKadd@users.noreply.github.com>
2022-01-15 14:14:32 +0100

    Add files via upload

---
+++ CPPkindaclean/car.cpp
@@ -0,0 +1,26 @@
+Car::Car(int id, int length) {
+    this->id = id;
+    this->length = length;
+}
+
+bool Car::ostatniaProsta() const {
+    return (this->seq == (this->length - 1));
+}
+
+bool Car::isInTransit(int second, int lengthOfCurrentSt) const {
+    if (this->lastCross == -1) {
+        return false;
+    } else {
+        return ((second - this->lastCross) < lengthOfCurrentSt);
+    }
+}
+
+int Car::cross(int time) {
+    this->seq += 1;
+    this->lastCross = time;
+    return this->road[this->seq];
+}
+
+void Car::reset() {
+    this->lastCross = -1;
+}
